feb31d83269d215ae1b269f6dfcbd066bc653786
ikeermelero <ikermelero57@gmail.com>
2026-05-06 17:26:10 +0200

    Lógica principal de la aplicación

--- src/App.jsx
+++ src/App.jsx
@@ -1,122 +1,86 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+
+const country = {
+  ad: "Andorra",
+  ae: "Emiratos Árabes Unidos",
+  af: "Afganistán",
+  ag: "Antigua y Barbuda",
+  ai: "Anguila",
+  al: "Albania",
+  am: "Armenia",
+  ao: "Angola",
+  aq: "Antártida",
+  ar: "Argentina",
+  as: "Samoa Americana",
+  at: "Austria",
+  au: "Australia",
+  aw: "Aruba",
+  ax: "Åland",
+  az: "Azerbaiyán",
+  ba: "Bosnia y Herzegovina",
+  bb: "Barbados",
+  bd: "Bangladesh",
+  be: "Bélgica",
+  bf: "Burkina Faso",
+  bg: "Bulgaria",
+  bh: "Baréin",
+  bi: "Burundi",
+  bj: "Benín",
+  bl: "San Bartolomé",
+  bm: "Bermudas",
+  bn: "Brunéi",
+};
+
+const shuffle = (x) => [...x].sort(() => Math.random() - 0.5);
+
+const getRandomRound = (arr) => {
+  const options = Object.entries(arr)
+    .sort(() => Math.random() - 0.5)
+    .slice(0, 4);
+  const correct = options[0];
+  const incorrect = options.slice(1, 4);
+  
+  console.log("Correcta:", correct)
+  console.log("Incorrecta:", incorrect)
+  console.log("Opciones:", options)
+  return { options, correct, incorrect };
+};
+
+function MyButton() {
+  return <button>I'm a button</button>;shuffle(options)
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [round, setRound] = useState(() => getRandomRound(country));
+  const [score, setScore] = useState(0);
+
+  const handleAnswer = (cn) => {
+    if (cn === round.correct[0]) {
+      //setScore((score) => score++);
+      console.log("acertaste");
+      setRound(getRandomRound())
+    } else {
+      console.log("no te desanimes");
+    }
+  };
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+      <h1>Hello Word!!</h1>
 
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
+          <img src={`https://flagcdn.com/160x120/${round.correct[0]}.png`} alt="Fotografía de bandera" />
           <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+      {shuffle(round.options).map((c) => (
+        <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded" key={c[0]} onClick={() => handleAnswer(c[0])}> {c[1]} </button>
+      ))}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
