31dbb01ca065ba04c074545dff6afb9743eceee1
Eli <hidragyrum@gmail.com>
2026-05-07 13:32:57 +0200

    comentarios sobre nombre de variables

--- src/components/FlagImage.jsx
+++ src/components/FlagImage.jsx
@@ -1,6 +1,6 @@
-// countryCode : round.correct[0]
+// constante countryCode se llama round.correct[0] en App component original
 
-export function FlagCard({ countryCode }) {
+export function FlagImage({ countryCode }) {
   return (
     <img 
       src={`https://flagcdn.com/160x120/${countryCode}.png`} 
